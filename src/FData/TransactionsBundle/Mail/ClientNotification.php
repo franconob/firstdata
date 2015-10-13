9acec232934d916b5a232d216163c1e29bf25f12
Franco Herrero <fherrero@spiralti.com>
2015-10-13 11:08:42 -0300

    se permiten ingresar varias direcciones de correo separadas por coma

--- src/FData/TransactionsBundle/Mail/ClientNotification.php
+++ src/FData/TransactionsBundle/Mail/ClientNotification.php
@@ -12,7 +12,6 @@ namespace FData\TransactionsBundle\Mail;
 use FData\SecurityBundle\User\User;
 use FData\TransactionsBundle\Transaction\Response;
 use FData\TransactionsBundle\Transaction\Transaction;
-use Knp\Bundle\SnappyBundle\Snappy\LoggableGenerator;
 use Openbuildings\Swiftmailer\CssInlinerPlugin;
 use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\Security\Core\SecurityContext;
@@ -55,26 +54,32 @@ class ClientNotification
     protected $crmDomain;
 
     /**
-     * @param \Swift_Mailer $mailer
-     * @param string $from
-     * @param TwigEngine $twig
+     * @param \Swift_Mailer   $mailer
+     * @param string          $from
+     * @param TwigEngine      $twig
      * @param SecurityContext $securityContext
-     * @param string $crmPath
-     * @param string $crmDomain
+     * @param string          $crmPath
+     * @param string          $crmDomain
      */
-    public function __construct(\Swift_Mailer $mailer, $from, TwigEngine $twig, SecurityContext $securityContext, $crmPath, $crmDomain)
-    {
-        $this->mailer = $mailer;
-        $this->from = $from;
-        $this->twig = $twig;
-        $this->user = $securityContext->getToken()->getUser();
+    public function __construct(
+        \Swift_Mailer $mailer,
+        $from,
+        TwigEngine $twig,
+        SecurityContext $securityContext,
+        $crmPath,
+        $crmDomain
+    ) {
+        $this->mailer    = $mailer;
+        $this->from      = $from;
+        $this->twig      = $twig;
+        $this->user      = $securityContext->getToken()->getUser();
         $this->crmDomain = $crmDomain;
-        $this->crmPath = $crmPath;
+        $this->crmPath   = $crmPath;
     }
 
     /**
      * @param Response $transaction
-     * @param string $mail
+     * @param string   $mail
      * @return int
      * @throws \Exception
      * @throws \Twig_Error
@@ -82,33 +87,44 @@ class ClientNotification
     public function createAndSend(Response $transaction, $mail)
     {
         $vars = [
-            'transactionType' => Transaction::$typeMap[(int)$transaction->get('transaction_type')],
-            'cardholderName' => $transaction->get('cardholder_name'),
-            'transactionTag' => $transaction->get('transaction_tag'),
-            'hotel' => $this->user->getHotel(),
-            'hotelDireccion' => $this->user->getExtraData('dir_calle') . ' ' . $this->user->getExtraData('dir_pobox'),
-            'hotelProvincia' => $this->user->getExtraData('dir_provincia'),
-            'hotelPais' => $this->user->getExtraData('dir_pais'),
-            'amount' => $transaction->get('amount'),
-            'currency' => $transaction->get('currency_code'),
-            'leyenda' => $this->user->getExtraData('leyenda_recibo'),
-            'referenceNo' => $transaction->get('reference_no'),
-            'bank_message' => $transaction->get('bank_message')
+            'transactionType' => Transaction::$typeMap[(int) $transaction->get('transaction_type')],
+            'cardholderName'  => $transaction->get('cardholder_name'),
+            'transactionTag'  => $transaction->get('transaction_tag'),
+            'hotel'           => $this->user->getHotel(),
+            'hotelDireccion'  => $this->user->getExtraData('dir_calle').' '.$this->user->getExtraData('dir_pobox'),
+            'hotelProvincia'  => $this->user->getExtraData('dir_provincia'),
+            'hotelPais'       => $this->user->getExtraData('dir_pais'),
+            'amount'          => $transaction->get('amount'),
+            'currency'        => $transaction->get('currency_code'),
+            'leyenda'         => $this->user->getExtraData('leyenda_recibo'),
+            'referenceNo'     => $transaction->get('reference_no'),
+            'bank_message'    => $transaction->get('bank_message'),
         ];
 
         if ($logo = $this->user->getExtraData('logo')) {
-            if (file_exists($this->crmPath . DIRECTORY_SEPARATOR . $logo)) {
+            if (file_exists($this->crmPath.DIRECTORY_SEPARATOR.$logo)) {
                 $vars['logo'] = $this->crmDomain.'/'.$logo;
             }
         }
 
-        $body = $this->renderBody($vars);
+        $body    = $this->renderBody($vars);
         $subject = $this->getSubject($transaction);
 
         $message = new \Swift_Message($subject, $body, 'text/html');
 
+        $emails = explode(',', $mail);
+
+        $emails = array_filter($emails);
+
+        $emails = array_map(
+            function ($email) {
+                return preg_match('/.+\@.+\..+/', $email);
+            },
+            $emails
+        );
+
         $message->setFrom($this->from);
-        $message->setTo($mail);
+        $message->setTo($emails);
 
         $this->mailer->registerPlugin(new CssInlinerPlugin());
 
@@ -132,7 +148,11 @@ class ClientNotification
      */
     public function getSubject(Response $transaction)
     {
-        return sprintf("%s - Your order %s has been paid successfully", $this->user->getHotel(), $transaction->get('transaction_tag'));
+        return sprintf(
+            "%s - Your order %s has been paid successfully",
+            $this->user->getHotel(),
+            $transaction->get('transaction_tag')
+        );
     }
 
     /**
